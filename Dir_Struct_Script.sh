377bfa9fb572dcef03bdceaffda0add8ce679b97
Kumar <Hemant.Kumar@adidas.com>
2023-09-22 23:35:23 +0530

    Dir Struture Creation

---
+++ Dir_Struct_Script.sh
@@ -0,0 +1,24 @@
+# Create a new directory for your DevOps Learning Platform
+mkdir DevOps-Learning-Platform
+
+# Navigate into the newly created directory
+cd DevOps-Learning-Platform
+
+# Create subdirectories for the project structure
+mkdir css js img content
+
+# Create an initial HTML file
+touch index.html
+
+# Create an initial CSS file
+touch css/styles.css
+
+# Create an initial JavaScript file (optional)
+touch js/script.js
+
+# Create an initial Markdown content file (you can add more later)
+touch content/lesson1.md
+
+# Create a _config.yml file for Jekyll (optional)
+touch _config.yml
+
